7482b7c4e5888639f303467860bfdfaca2becf59
Omar Eldeeb <omareldeeb00@live.com>
2025-07-08 09:38:29 +0200

    Update run_mlff_job script

---
+++ run_mlff_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=mlff-debug
+#SBATCH --output=tests/results/mlff/slurm-%j.out
+#SBATCH --error=tests/results/mlff/slurm-%j.err
+#SBATCH --partition=gpu_a100
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --time=03:00:00
+
+cd $SLURM_SUBMIT_DIR
+
+# Activate your environment
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate s2gnn
+
+# Optional: confirm GPU
+# nvidia-smi
+
+# Run your script
+python main.py --cfg configs/mlff/mlff-gemnet-layers.yaml \
+               out_dir tests/results/mlff \
+               wandb.use True \
+               seed 1
